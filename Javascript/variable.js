dfbd9e9028f68ecb09969ec44e94642e86137084
unknown <gnansidda@gmail.com>
2019-08-06 10:15:38 +0530

    feat:added javascripts examples

---
+++ Javascript/variable.js
@@ -0,0 +1,25 @@
+var a = 10;
+console.log("a value="+a);//printing a value
+console.log("Type of a value="+typeof(a));//printing type of a
+//string
+var b = "deepak";
+console.log("b value=" +b);
+console.log("type of b="+typeof(b));
+
+// boolean
+var bool = true;
+console.log("bool value="+bool);
+console.log("type od bool="+typeof(bool));
+
+//Array
+var arr=[1,2,3,4];
+console.log("arr value="+arr);
+console.log("type of arr="+typeof(arr));
+console.log("length of arr="+arr.length);
+
+//object
+var obj = {"id":101, "name":"Deepak","Emp Id": "300822"};
+console.log("obj value="+JSON.stringify(obj));
+console.log("type of obj="+typeof(obj));
+console.log("id value:"+obj.id);
+console.log("name value:"+obj.name);
